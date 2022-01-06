aae08456976672c59fd7f60192f7844a7ccd6dce
Emilie Duffin <57412715+ermduffin@users.noreply.github.com>
2022-01-06 13:35:01 -0500

    Delete export/windows directory

--- export/windows/bin/assets/data/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tile_map" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="../images/tile_map.png" width="160" height="64"/>
-</tileset>
